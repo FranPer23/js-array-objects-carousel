733575bc41668a6cc2e4fd41daa178cfbcf63d62
FranPer23 <francescaprt@yahoo.com>
2023-03-23 16:29:25 +0100

    prova

---
+++ js/prova.js
@@ -0,0 +1,33 @@
+const images = [
+    {
+        image: 'img/img_1.jpg',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/img_2.jpg',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/img_3.jpg',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/img_4.jpg',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/img_5.jpg',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const newArrayOfImages = [];
+
+images.forEach((curImg) => {
+    const nextImg = curImg + 1;
+    newArrayOfImages.push(nextImg)
+    console.log(curImg.image, curImg.text);
+  
+})
+
